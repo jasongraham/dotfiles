6e54df6a1e2876aa7462bcc5ff00e4c4e9d1fdb4
Jason Graham <jason@the-graham.com>
2012-04-07 16:15:38 -0700

    Fix readme and setup exclusion

--- setup.sh
+++ setup.sh
@@ -2,12 +2,7 @@
 # This is a script to symlink all of the configuration files found in this
 # repository into the user's home directory.
 
-for f in * ; do
-
-    # don't link this script
-    if [ ["$f" == $0] -o ["$f" == "Readme.md"] ] ; then
-        continue
-    fi
+for f in `ls | egrep -v 'setup|Readme'`; do
 
     # remove old config files, replace with links to here
     rm $HOME/.$f
