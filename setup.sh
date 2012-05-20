706379fc28abddc43979a46c727aae02e51d6cda
Jason Graham <jason@the-graham.com>
2012-05-20 14:45:04 -0700

    Make calling setup.sh as a subscript easier

--- setup.sh
+++ setup.sh
@@ -2,11 +2,17 @@
 # This is a script to symlink all of the configuration files found in this
 # repository into the user's home directory.
 
+# Get the directory name of the setup script
+DIR="$( cd "$(dirname "$0")" && pwd)"
+
+pushd $DIR > /dev/null
+
 for f in `ls | egrep -v 'setup|Readme'`; do
 
     # remove old config files, replace with links to here
     rm $HOME/.$f
-    ln -s $PWD/$f $HOME/.$f
+    ln -s $DIR/$f $HOME/.$f
 
 done
 
+popd > /dev/null
