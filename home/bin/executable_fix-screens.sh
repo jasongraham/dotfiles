55e3b64e45103fac44048336178c13f5a758acfd
Jason Graham <j.and.w.graham@gmail.com>
2024-04-27 00:39:14 -0700

    Remove remnants of Xorg based configs

--- home/bin/executable_fix-screens.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nvidia-settings --assign "CurrentMetaMode=DPY-7: nvidia-auto-select @1920x1080 +0+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0}, DPY-5: nvidia-auto-select @1920x1080 +1920+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0}"
