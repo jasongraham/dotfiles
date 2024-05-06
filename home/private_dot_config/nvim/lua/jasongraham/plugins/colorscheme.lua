324dbaae26e59790a3ee46155677fc0c5bbdc86b
Jason Graham <j.and.w.graham@gmail.com>
2024-05-05 23:20:20 -0700

    Bold and italic in vim colorscheme

--- home/private_dot_config/nvim/lua/jasongraham/plugins/colorscheme.lua
+++ home/private_dot_config/nvim/lua/jasongraham/plugins/colorscheme.lua
@@ -6,6 +6,8 @@ return {
 			-- load the colorscheme here
 			vim.g.gruvbox_material_foreground = "material"
 			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_enable_bold = 1
+			vim.g.gruvbox_material_enable_italic = 1
 			vim.cmd.colorscheme("gruvbox-material")
 		end,
 	},
