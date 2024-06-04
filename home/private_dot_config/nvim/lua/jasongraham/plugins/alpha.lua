84cc509ba197b1d951ec2faf6977149e8f93fe71
Jason Graham <j.and.w.graham@gmail.com>
2024-06-03 21:25:33 -0700

    Nvimtree -> Neotree

--- home/private_dot_config/nvim/lua/jasongraham/plugins/alpha.lua
+++ home/private_dot_config/nvim/lua/jasongraham/plugins/alpha.lua
@@ -20,7 +20,7 @@ return {
 		-- Set menu
 		dashboard.section.buttons.val = {
 			dashboard.button("e", " > New File", "<cmd>ene<CR>"),
-			dashboard.button("<leader> ee", " > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("<leader> ee", " > Toggle file explorer", "<cmd>Neotree show<CR>"),
 			dashboard.button("<leader> ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("<leader> fr", "󰁯 > Find Recent Files", "<cmd>Telescope oldfiles<CR>"),
 			dashboard.button("<leader> fs", " > Find String", "<cmd>Telescope live_grep<CR>"),
