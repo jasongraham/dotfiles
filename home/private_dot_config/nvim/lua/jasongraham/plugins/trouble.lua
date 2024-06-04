06ff44cdd1e218109186d4d334aa683f685c4ff1
Jason Graham <j.and.w.graham@gmail.com>
2024-06-03 18:12:26 -0700

    Update trouble.nvim for latest version

--- home/private_dot_config/nvim/lua/jasongraham/plugins/trouble.lua
+++ home/private_dot_config/nvim/lua/jasongraham/plugins/trouble.lua
@@ -2,12 +2,20 @@ return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
 	event = { "BufReadPre", "BufNewFile" },
+	version = "^3",
+	cmd = "Trouble",
 	keys = {
-		{ "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-		{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-		{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-		{ "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-		{ "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-		{ "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Buffer Diagnostics (Trouble)" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle<CR>", desc = "Project Diagnostics (Trouble)" },
+		{ "<leader>xq", "<cmd>Trouble qflist toggle<CR>", desc = "Open trouble quickfix list" },
+		{
+			"<leader>xl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
+			desc = "LSP definitions / refs / ...",
+		},
+		{ "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
 	},
+
+	-- Required for actually being ready and available
+	config = true,
 }
