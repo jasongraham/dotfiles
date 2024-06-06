b4559e23ad2a45e4a4fbc59c2e4f58628c1f258f
Jason Graham <j.and.w.graham@gmail.com>
2024-06-05 22:24:46 -0700

    Add shortcut to edit vim config to startup screen

--- home/private_dot_config/nvim/lua/jasongraham/plugins/alpha.lua
+++ home/private_dot_config/nvim/lua/jasongraham/plugins/alpha.lua
@@ -19,12 +19,13 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", " > New File", "<cmd>ene<CR>"),
-			dashboard.button("<leader> ee", " > Toggle file explorer", "<cmd>Neotree show<CR>"),
-			dashboard.button("<leader> ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("<leader> fr", "󰁯 > Find Recent Files", "<cmd>Telescope oldfiles<CR>"),
-			dashboard.button("<leader> fs", " > Find String", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("e", " > New File", ":ene<CR>"),
+			dashboard.button("<leader> ee", " > Toggle file explorer", ":Neotree show<CR>"),
+			dashboard.button("<leader> ff", "󰱼 > Find File", ":Telescope find_files<CR>"),
+			dashboard.button("<leader> fr", "󰁯 > Find Recent Files", ":Telescope oldfiles<CR>"),
+			dashboard.button("<leader> fs", " > Find String", ":Telescope live_grep<CR>"),
+			dashboard.button("s", " > Settings", ":e $MYVIMRC | :cd %:p:h | :Neotree<CR>"),
+			dashboard.button("q", " > Quit NVIM", ":qa<CR>"),
 		}
 
 		-- Send config to alpha
