0b9950f267ae96fb9de89c9ea4690e4f78e6d338
Jason Graham <j.and.w.graham@gmail.com>
2024-07-30 21:04:18 -0700

    Keep newest aurto packages, remove old ones

--- home/bin/executable_pkgclean.sh
+++ home/bin/executable_pkgclean.sh
@@ -10,12 +10,21 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 for dir in /var/cache/pacman/* ; do
-    if [[ "$dir" == *aurto ]] ; then
-        echo "####################################################################"
-        echo "Skipping $dir"
-        continue
-    fi
     echo "####################################################################"
-    echo "Cleaning $dir"
-    pkgcacheclean --verbose --cachedir "$dir"
+    case $dir in
+        *aurto)
+            echo "Cleaning old packages from $dir"
+            # Keep latest version of everything, even if not installed.
+            # Use aurto to manage removing unwanted packages from here.
+            pkgcacheclean \
+                --verbose \
+                --all-as-installed \
+                --cachedir "$dir"
+            ;;
+        *)
+            echo "Cleaning old and uninstalled packages from $dir"
+            pkgcacheclean \
+                --verbose \
+                --cachedir "$dir"
+    esac
 done
