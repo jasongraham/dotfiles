f249bc361be6fb1fe77c3d1ef09a6856fb5eac6e
Jason Graham <j.and.w.graham@gmail.com>
2024-09-03 10:32:45 -0700

    Human readable output from pkgcacheclean script

--- home/bin/executable_pkgclean.sh
+++ home/bin/executable_pkgclean.sh
@@ -9,6 +9,8 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+readonly KEEP_PACKAGES=2
+
 for dir in /var/cache/pacman/* ; do
     echo "####################################################################"
     case $dir in
@@ -17,14 +19,18 @@ for dir in /var/cache/pacman/* ; do
             # Keep latest version of everything, even if not installed.
             # Use aurto to manage removing unwanted packages from here.
             pkgcacheclean \
-                --verbose \
                 --all-as-installed \
-                --cachedir "$dir"
+                --verbose \
+                --human-readable \
+                --cachedir "$dir" \
+                $KEEP_PACKAGES
             ;;
         *)
             echo "Cleaning old and uninstalled packages from $dir"
             pkgcacheclean \
                 --verbose \
-                --cachedir "$dir"
+                --human-readable \
+                --cachedir "$dir" \
+                $KEEP_PACKAGES
     esac
 done
