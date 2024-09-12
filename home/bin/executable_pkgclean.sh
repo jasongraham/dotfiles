1cf10ef2da83fe272c4ec3817f7eb38ec3d4cd9c
Jason Graham <j.and.w.graham@gmail.com>
2024-09-11 21:54:36 -0700

    Refactor pkgclean script options

--- home/bin/executable_pkgclean.sh
+++ home/bin/executable_pkgclean.sh
@@ -13,24 +13,19 @@ readonly KEEP_PACKAGES=2
 
 for dir in /var/cache/pacman/* ; do
     echo "####################################################################"
+    OPTS=( \
+        --verbose \
+        --human-readable \
+        --cachedir "$dir" \
+    )
     case $dir in
         *aurto)
-            echo "Cleaning old packages from $dir"
             # Keep latest version of everything, even if not installed.
-            # Use aurto to manage removing unwanted packages from here.
-            pkgcacheclean \
-                --all-as-installed \
-                --verbose \
-                --human-readable \
-                --cachedir "$dir" \
-                $KEEP_PACKAGES
+            echo "Cleaning old packages from $dir"
+            OPTS=("${OPTS[@]}" --all-as-installed)
             ;;
         *)
             echo "Cleaning old and uninstalled packages from $dir"
-            pkgcacheclean \
-                --verbose \
-                --human-readable \
-                --cachedir "$dir" \
-                $KEEP_PACKAGES
     esac
+    pkgcacheclean "${OPTS[@]}" $KEEP_PACKAGES
 done
