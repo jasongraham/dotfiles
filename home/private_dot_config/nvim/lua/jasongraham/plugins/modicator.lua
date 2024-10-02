e16ebedea36cebfdb78052252cfe40d0774388b8
Jason Graham <j.and.w.graham@gmail.com>
2024-10-01 20:29:19 -0700

    Empty opts for modicator really slows down startup

--- home/private_dot_config/nvim/lua/jasongraham/plugins/modicator.lua
+++ home/private_dot_config/nvim/lua/jasongraham/plugins/modicator.lua
@@ -11,11 +11,11 @@ return {
         vim.o.number = true
         vim.o.termguicolors = true
     end,
-    opts = {
-        -- Warn if any required option above is missing. May emit false positives
-        -- if some other plugin modifies them, which in that case you can just
-        -- ignore. Feel free to remove this line after you've gotten Modicator to
-        -- work properly.
-        -- show_warnings = true,
-    },
+    -- opts = {
+    --     -- Warn if any required option above is missing. May emit false positives
+    --     -- if some other plugin modifies them, which in that case you can just
+    --     -- ignore. Feel free to remove this line after you've gotten Modicator to
+    --     -- work properly.
+    --     -- show_warnings = true,
+    -- },
 }
