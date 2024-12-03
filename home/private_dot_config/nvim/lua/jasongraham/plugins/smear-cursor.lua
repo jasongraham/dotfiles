8adf86848c8f99fdf144b21984e58dd5ae8f0a06
Jason Graham <j.and.w.graham@gmail.com>
2024-12-02 19:17:39 -0800

    Use default settings for vim plugin smear-cursor

--- home/private_dot_config/nvim/lua/jasongraham/plugins/smear-cursor.lua
+++ home/private_dot_config/nvim/lua/jasongraham/plugins/smear-cursor.lua
@@ -1,20 +1,5 @@
 return {
     "sphamba/smear-cursor.nvim",
     event = { "VeryLazy" },
-
-    opts = {
-        -- Cursor color. Defaults to Normal foreground color
-        -- cursor_color = '#d3cdc3',
-
-        -- Use floating windows to display smears outside buffers.
-        -- May have performance issues with other plugins.
-        use_floating_windows = true,
-
-        -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-        -- Smears will blend better on all backgrounds.
-        legacy_computing_symbols_support = false,
-
-        -- Attempt to hide the real cursor when smearing.
-        hide_target_hack = true,
-    },
+    config = true,
 }
