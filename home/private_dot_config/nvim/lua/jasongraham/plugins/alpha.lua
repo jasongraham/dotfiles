311dc4fe0cddbc86e5cf9f307146d7d7d4968409
Jason Graham <j.and.w.graham@gmail.com>
2024-12-06 15:27:49 -0800

    Alpha.nvim enhancements with lazy.nvim

--- home/private_dot_config/nvim/lua/jasongraham/plugins/alpha.lua
+++ home/private_dot_config/nvim/lua/jasongraham/plugins/alpha.lua
@@ -1,8 +1,7 @@
 return {
     "goolord/alpha-nvim",
     event = "VimEnter",
-    config = function()
-        local alpha = require("alpha")
+    opts = function()
         local dashboard = require("alpha.themes.dashboard")
 
         -- Set header
@@ -18,20 +17,52 @@ return {
         }
 
         -- Set menu
+        -- stylua: ignore
         dashboard.section.buttons.val = {
-            dashboard.button("e", " > New File", ":ene<CR>"),
-            dashboard.button("<leader> ee", " > Toggle file explorer", ":Neotree show<CR>"),
-            dashboard.button("<leader> ff", "󰱼 > Find File", ":Telescope find_files<CR>"),
-            dashboard.button("<leader> fr", "󰁯 > Find Recent Files", ":Telescope oldfiles<CR>"),
-            dashboard.button("<leader> fs", " > Find String", ":Telescope live_grep<CR>"),
-            dashboard.button("s", " > Settings", ":e $MYVIMRC | :cd %:p:h | :Neotree<CR>"),
-            dashboard.button("q", " > Quit NVIM", ":qa<CR>"),
+            dashboard.button("e",           " > New File",             ":ene<CR>"),
+            dashboard.button("<leader> ff", "󰱼 > Find File",            ":Telescope find_files<CR>"),
+            dashboard.button("<leader> fr", "󰁯 > Find Recent Files",    ":Telescope oldfiles<CR>"),
+            dashboard.button("<leader> fs", " > Find String",          ":Telescope live_grep<CR>"),
+            dashboard.button("s",           " > Settings",             ":e $MYVIMRC | :cd %:p:h | :Neotree<CR>"),
+            dashboard.button("q",           " > Quit NVIM",            ":qa<CR>"),
         }
+        return dashboard
+    end,
+    config = function(_, dashboard)
+        -- close Lazy and re-open when the dashboard is ready
+        if vim.o.filetype == "lazy" then
+            vim.cmd.close()
+            vim.api.nvim_create_autocmd("User", {
+                once = true,
+                pattern = "AlphaReady",
+                callback = function()
+                    require("lazy").show()
+                end,
+            })
+        end
 
         -- Send config to alpha
-        alpha.setup(dashboard.opts)
+        require("alpha").setup(dashboard.opts)
 
         -- Disable folding on alpha buffer
         vim.cmd.autocmd("Filetype", "alpha", "setlocal", "nofoldenable")
+
+        -- Show lazy.nvim startup time
+        vim.api.nvim_create_autocmd("User", {
+            once = true,
+            pattern = "LazyVimStarted",
+            callback = function()
+                local stats = require("lazy").stats()
+                local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                dashboard.section.footer.val = "⚡ Neovim loaded "
+                    .. stats.loaded
+                    .. "/"
+                    .. stats.count
+                    .. " plugins in "
+                    .. ms
+                    .. "ms"
+                pcall(vim.cmd.AlphaRedraw)
+            end,
+        })
     end,
 }
