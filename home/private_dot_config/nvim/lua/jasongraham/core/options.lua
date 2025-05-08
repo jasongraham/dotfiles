1775df5a326ac6d2f5cf9aafe25a0395a39563cd
Jason Graham <j.and.w.graham@gmail.com>
2025-05-08 15:28:26 -0700

    vim.opt -> vim.o in options.lua

--- home/private_dot_config/nvim/lua/jasongraham/core/options.lua
+++ home/private_dot_config/nvim/lua/jasongraham/core/options.lua
@@ -1,43 +1,44 @@
 vim.g.netrw_liststyle = 3
 
-local opt = vim.opt -- for conciseness
-
 -- line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+vim.o.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 4 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
+vim.o.tabstop = 4 -- 2 spaces for tabs (prettier default)
+vim.o.shiftwidth = 4 -- 2 spaces for indent width
+vim.o.expandtab = true -- expand tab to spaces
+vim.o.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
-opt.wrap = false -- disable line wrapping
+vim.o.wrap = false -- disable line wrapping
 
 -- search settings
-opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+vim.o.ignorecase = true -- ignore case when searching
+vim.o.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
-opt.cursorline = true -- highlight the current cursor line
+vim.o.cursorline = true -- highlight the current cursor line
 
 -- appearance
-opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
-opt.colorcolumn = "100"
+vim.o.termguicolors = true
+vim.o.background = "dark" -- colorschemes that can be light or dark will be made dark
+vim.o.signcolumn = "yes" -- show sign column so that text doesn't shift
+vim.o.colorcolumn = "100"
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+vim.o.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+-- Schedule the settings after 'UiEnter to reduce start-up time'
+vim.schedule(function()
+    vim.o.clipboard = "unnamedplus" -- use system clipboard as default register
+end)
 
 -- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+vim.o.splitright = true -- split vertical window to the right
+vim.o.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
-opt.swapfile = false
+vim.o.swapfile = false
 
 vim.diagnostic.config({ jump = { float = true } })
