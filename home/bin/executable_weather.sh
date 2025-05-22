f49dc1305a1a27a652269489ae805b4a67326acb
Jason Graham <j.and.w.graham@gmail.com>
2025-05-21 22:50:04 -0700

    Have weather script wait for network to be up before starting

--- home/bin/executable_weather.sh
+++ home/bin/executable_weather.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# TODO: The "lock" file check and creation is not atomic, and does not ensure
+# only a single instance of this script is running.
 
 set -eu -o pipefail
 
 readonly FILE="/tmp/weather_cached"
 readonly INTERVAL=600  # seconds
+
+while ! systemctl -q is-active network.target ; do
+    sleep 1
+done
 
 [ -f "$FILE" ] && \
     notify-send -u normal "weather wttr fetch" "cache file already exists" && \
