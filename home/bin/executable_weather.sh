c966657dda2a7d511d8a717b01d7b067de332151
Jason Graham <j.and.w.graham@gmail.com>
2025-05-22 23:50:18 -0700

    Use pid to ensure only one instance of weather script is running

--- home/bin/executable_weather.sh
+++ home/bin/executable_weather.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-# TODO: The "lock" file check and creation is not atomic, and does not ensure
-# only a single instance of this script is running.
 
 set -eu -o pipefail
 
 readonly FILE="/tmp/weather_cached"
 readonly INTERVAL=600  # seconds
 
-while ! systemctl -q is-active network.target ; do
+trap 'notify-send -u normal "weather wttr fetch" "exited unexpectedly"' EXIT
+
+while ! systemctl -q is-active network-online.target ; do
     sleep 1
 done
-
-[ -f "$FILE" ] && \
-    notify-send -u normal "weather wttr fetch" "cache file already exists" && \
-    exit
 
 while true; do
     curl wttr.in/?format=1 --silent --output "$FILE"
