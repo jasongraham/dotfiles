89f245773f69523c84a2f2c2fe4fbe2f5024c649
Jason Graham <j.and.w.graham@gmail.com>
2025-08-05 20:21:28 -0700

    Weather script updates
    Show time of latest update to weather
    
    Notify and continue after a curl failure

--- home/bin/executable_weather.sh
+++ home/bin/executable_weather.sh
@@ -3,8 +3,8 @@
 set -eu -o pipefail
 
 readonly FILE="/tmp/weather_cached"
-readonly INTERVAL_MIN=600  # seconds
-readonly INTERVAL_MAX=1200  # seconds
+readonly INTERVAL_MIN=900  # seconds
+readonly INTERVAL_MAX=1800  # seconds
 
 trap 'notify-send -u normal "weather wttr fetch" "exited unexpectedly"' EXIT
 
@@ -13,6 +13,15 @@ while ! systemctl -q is-active network-online.target ; do
 done
 
 while true; do
-    curl wttr.in/?format=1 --silent --output "$FILE"
+    if ! curl wttr.in/?format=1 --silent --output "$FILE" ; then
+        # Don't exit on a curl failure, but do notify
+        notify-send -u normal "weather wttr fetch" "curl error"
+    else
+        # Include time to see how stale the reading is
+        truncate -s-1 "$FILE"
+        echo -n " @ " >> "$FILE"
+        date -r "$FILE" "+%I:%M%p" >> "$FILE"
+    fi
+    # Regardless, try again later
     sleep "$((RANDOM % (INTERVAL_MAX-INTERVAL_MAX+1)+INTERVAL_MIN))"
 done
